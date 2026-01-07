6f251d809450967e3f2611ec877c1efcc1fd7bb0
princeps091-binf <princep091@gmail.com>
2026-01-07 13:17:16 +0100

    implemented TAD-domain like plotting for read clusters

--- TFHclust/src/explore_pol2.py
+++ TFHclust/src/explore_pol2.py
@@ -3,12 +3,17 @@ import pandas as pd
 import numpy as np
 import bioframe as bf
 import matplotlib.pyplot as plt
+from matplotlib.cm import viridis
 import os
 from importlib import reload
 import detect_cluster
 reload(detect_cluster)
 import integrate_replicate
 reload(integrate_replicate)
+
+import matplotlib.patches as pat  # Patches like pat.Polygon()
+from matplotlib.collections import PolyCollection  # Collections of patches
+
 #%%
 ctrl_bam = "/home/vipink/Documents/TFHclust/TFHclust/data/POLR2A/ctrl/ENCFF062SCH.bam"
 rep1_obs_bam = "/home/vipink/Documents/TFHclust/TFHclust/data/POLR2A/ENCFF343QJV.bam"
@@ -45,4 +50,29 @@ merged_hic_clustering.match_rep_cluster()
 merged_hic_clustering.evaluate_overlap_significance(10)
 merged_hic_clustering.filter_significant_overlap_cluster(0.5,0.5)
 # %%
-merged_hic_clustering.inter_rep_match_tbl.sort_values('w')
+merged_hic_clustering.inter_rep_match_tbl.plot.scatter(x='zscore_rep1',y='zscore_rep2')
+
+#%%
+triangles = []
+for _,t in merged_hic_clustering.inter_rep_match_tbl.sort_values('norm_lvl_rep1').iterrows():
+    xmid = t.start + (t.w/2)  # Middle x-coord
+    xleft = t.start
+    xright = t.end
+
+    y1 = 0  # y-coords
+    y2 = (t.w/2)
+
+    coordinates = [[xleft, y1], [xright, y1], [xmid, y2]]
+
+    print(coordinates) 
+    triangles.append(coordinates)  # Append to collection
+
+z = (merged_hic_clustering.inter_rep_match_tbl.sort_values('norm_lvl_rep1').norm_lvl_rep2 + merged_hic_clustering.inter_rep_match_tbl.sort_values('norm_lvl_rep1').norm_lvl_rep1)/2
+collec = PolyCollection(triangles, array=z, cmap=viridis)
+
+fig, ax = plt.subplots(1, 1)
+ax.add_collection(collec)  # Plot polygon collection
+ax.autoscale_view()
+plt.show()
+
+# %%
